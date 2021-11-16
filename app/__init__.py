01b8415be8a9f1d7692c052a1dba42b77fabf45f
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-16 10:37:57 -0500

    initialize login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -17,6 +21,7 @@ def create_app(config_name):
     #Initializing Flask Extenstion
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
