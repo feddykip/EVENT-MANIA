b62f3d90802789e80c123b8c726b55b5337237e6
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-16 10:12:46 -0500

    create password function

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from .import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 #...
 
@@ -10,6 +11,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
 
 class Event(db.Model):
     """
