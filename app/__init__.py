d2376734867bf4cd1ff781bcde7865e1d10a934d
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-16 10:20:34 -0500

    register blueprint instance

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
+
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    #initializing the application
+    app = Flask(__name__)
+
+    #Initializing Flask Extenstion
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+
+
+    
+
+    
+
+    return app
