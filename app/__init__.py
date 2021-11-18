01a502fffef6d1df3c359eb0ad3e38a629fdd92b
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-18 01:14:34 -0500

    register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,46 @@
 from flask import Flask
-from flask.helpers import url_for
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-
-
-
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
 
 def create_app(config_name):
-    #initializing the application
+
     app = Flask(__name__)
 
-    #Initializing Flask Extenstion
+    #Creating application configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    #Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    mail.init_app(app)
 
 
+    #configure UploadSet
+    configure_uploads(app,photos)
 
+    from . auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authentication')
 
-    
+    #Registering the blueprint
+    from . main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-    
 
     return app
